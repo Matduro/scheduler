ab2a50721091847dbe3111ab8121084fc5ec9734
Mathieu <mathieurousseau123@gmail.com>
2021-04-10 13:55:48 -0700

    fixed webpack errors

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,22 @@
 const path = require("path");
 
 module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  });
   return {
     ...config,
     resolve: {
       ...config.resolve,
-      modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules]
-    }
+      modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules],
+    },
   };
 };
